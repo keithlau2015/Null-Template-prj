f452a3d1821f6d3058b2aefc75a259685ea2d6ce
keithlau2015 <s101315@gmail.com>
2026-08-23 22:47:38 +0800

    Revert "Add PortalModule with 2D teleport system and test scene."
    This reverts commit 5c888c34e84254d85469296ffc6eacd2b24c9308.

--- Assets/Script/PortalModule/IPortalTeleportable.cs
+++
@@ -1,8 +0,0 @@
-namespace PortalModule
-{
-    public interface IPortalTeleportable
-    {
-        bool OnBeforePortalTeleport(PortalTeleportContext context);
-        void OnAfterPortalTeleport(PortalTeleportContext context);
-    }
-}
